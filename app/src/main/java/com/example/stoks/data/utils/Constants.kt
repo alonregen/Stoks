459ce1e6604062669dddd2cb128cfe6451f31870
Alon regenbogen <regenbogen.alon@post.runi.ac.il>
2023-07-02 22:04:08 +0300

    first commit

---
+++ app/src/main/java/com/example/stoks/data/utils/Constants.kt
@@ -0,0 +1,12 @@
+package com.example.stoks.data.utils
+
+
+class Constants {
+
+    companion object {
+        const val BASE_URL = "https://finnhub.io/api/v1/"
+        const val API_KEY = "ci7hvuhr01qni8lh9vr0ci7hvuhr01qni8lh9vrg"
+        const val SYMBOL_QUERY_PARAM = "symbol"
+        const val TOKEN_QUERY_PARAM = "token"
+    }
+}
